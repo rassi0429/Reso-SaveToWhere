e7611bb85fde14f9e2698afaf52b5861ac912df8
rassi0429 <taitokokoa+rassi@gmail.com>
2023-09-27 15:10:05 +0900

    feat: 2.0.0

---
+++ SaveToWhere/SaveToWhere.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using FrooxEngine;
+using System.Collections.Generic;
+using ResoniteModLoader;
+using Elements.Core;
+
+namespace SaveToWhere
+{
+    public class SaveToWhere : ResoniteMod
+    {
+        public override string Name => "SaveToWhere";
+        public override string Author => "kka429";
+        public override string Version => "2.0.0";
+        public override string Link => "https://github.com/rassi0429/Reso-SaveToWhere"; // this line is optional and can be omitted
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("dev.kokoa.savetowhere");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Elements.Core.LocaleHelper), "AsLocaleKey", new Type[] { typeof(string), typeof(bool), typeof(Dictionary<string, object>) })]
+        class Patch
+        {
+            static void Postfix(ref LocaleString __result, string str)
+            {
+                if(str == "Interaction.SaveToInventory")
+                {                 
+                    __result = "Save To : \n /" + InventoryBrowser.CurrentUserspaceInventory?.CurrentPath.Replace("\\","/");
+                }
+            }
+        }
+    }
+}
